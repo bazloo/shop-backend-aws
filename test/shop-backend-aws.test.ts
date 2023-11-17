0fe792d365e25d8fcc7384fdf6030c9d3746e888
basileus <timberspirit@gmail.com>
2023-11-18 01:12:15 +0200

    feat(task-2): added basic configurations

---
+++ test/shop-backend-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ShopBackendAws from '../lib/shop-backend-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/shop-backend-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ShopBackendAws.ShopBackendAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
