b2840b42927b97eda12b05855762154c1318bcf7
basileus <timberspirit@gmail.com>
2023-11-25 01:13:01 +0200

    feat(task-4): added dynamodb tables

--- lib/shop-backend-aws-stack.ts
+++ lib/shop-backend-aws-stack.ts
@@ -6,10 +6,19 @@ import {
   RestApi,
 } from 'aws-cdk-lib/aws-apigateway';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { AttributeType, Table, BillingMode } from "aws-cdk-lib/aws-dynamodb";
 
 export class ProductService extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const productsTable = Table.fromTableName(this, 'Products-table', 'Products'); // TODO refactor
+
+    const stocksTable = new Table(this, 'DbTable', {
+      partitionKey: { name: 'id', type: AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
 
     const api = new RestApi(this, 'RestAPI', {
       restApiName: 'ProductServiceAPI',
@@ -22,6 +31,10 @@ export class ProductService extends Stack {
     const getProductsList = new NodejsFunction(this, 'GetProductsList', {
       entry: 'resources/route-handlers/products.ts',
       handler: 'handler',
+      environment: {
+        PRODUCTS_TABLE_NAME: 'Products', // TODO refactor
+        STOCKS_TABLE_NAME: stocksTable.tableName,
+      },
     });
 
     const getProductById = new NodejsFunction(this, 'GetProductsById', {
@@ -36,6 +49,9 @@ export class ProductService extends Stack {
     const productIntegration = new LambdaIntegration(getProductById);
 
     productList.addMethod('GET', productListIntegration);
-    productById.addMethod('GET', productIntegration,);
+    productById.addMethod('GET', productIntegration);
+
+    productsTable.grantReadData(getProductsList);
+    stocksTable.grantReadData(getProductsList);
   }
 }
