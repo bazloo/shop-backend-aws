9fca538add52cedd397027312c9fe7f0d23c2673
basileus <timberspirit@gmail.com>
2023-12-03 18:18:26 +0200

    feat(task-5): added additional logs

--- src/import-service/lambdas/importProductFile.ts
+++ src/import-service/lambdas/importProductFile.ts
@@ -8,9 +8,10 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 const s3Client = new S3Client();
 
 export const handler = async (event: APIGatewayProxyEvent) => {
-    //console.log(`IMPORT with name parameter: ${event.queryStringParameters?.name}`);
+    console.log(`IMPORT: started import for file with name: ${event.queryStringParameters?.name}`);
     const fileName = event.queryStringParameters?.name;
     if (!fileName) {
+        console.log(`IMPORT: query parameter 'name' is undefined`);
         return {
             headers: {
                 'Access-Control-Allow-Origin': '*',
@@ -30,6 +31,8 @@ export const handler = async (event: APIGatewayProxyEvent) => {
         const signedUrl = await getSignedUrl(s3Client, putCommand, {
             expiresIn: 300,
         });
+
+        console.log(`IMPORT: successfully created signed url for file upload`);
 
         return {
             headers: {
