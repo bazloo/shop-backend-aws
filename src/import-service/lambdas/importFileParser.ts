cb3b752e1d7c1732d24136174f89917c58c5f2ff
basileus <timberspirit@gmail.com>
2023-12-03 18:12:17 +0200

    feat(task-5): corrections

--- src/import-service/lambdas/importFileParser.ts
+++ src/import-service/lambdas/importFileParser.ts
@@ -17,14 +17,15 @@ export const handler = async (event: S3Event) => {
     console.log(`PARSER: file parser was triggered`);
     try {
         for await (const record of event.Records) {
-            const { key: originFilePath } = record.s3.object
+            const { key: originFilePath } = record.s3.object;
+            const { name: bucketName } = record.s3.bucket;
             const [_, fileName] = originFilePath.split('/');
 
             console.log(`PARSER: started to parse file: ${fileName}`);
             const { Body } = await s3Client.send(
                 new GetObjectCommand({
-                    Bucket: record.s3.bucket.name,
-                    Key: record.s3.object.key,
+                    Bucket: bucketName,
+                    Key: originFilePath,
                 })
             )
 
@@ -53,7 +54,7 @@ export const handler = async (event: S3Event) => {
                 await s3Client.send(
                     new CopyObjectCommand({
                         CopySource: `${record.s3.bucket.name}/${originFilePath}`,
-                        Bucket: record.s3.bucket.name,
+                        Bucket: bucketName,
                         Key: `${PARSED_FOLDER_NAME}/${fileName}`,
                     }),
                 );
@@ -62,7 +63,7 @@ export const handler = async (event: S3Event) => {
 
                 await s3Client.send(
                     new DeleteObjectCommand({
-                        Bucket: record.s3.bucket.name,
+                        Bucket: bucketName,
                         Key: originFilePath,
                     }),
                 );
